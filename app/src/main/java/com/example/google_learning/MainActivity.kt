641e864530147193c03fb35223491b64ccf9ffcf
HirunaWeerakoon <it24610788@my.sliit.lk>
2025-12-04 20:28:44 +0530

    first button in my life

---
+++ app/src/main/java/com/example/google_learning/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.google_learning
+
+import android.os.Bundle
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import android.view.Surface
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.google_learning.ui.theme.Google_learningTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Google_learningTheme {
+                var count by remember{
+                    mutableStateOf(0)
+                }
+                Column(
+                    modifier= Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(
+                        text=count.toString(),
+                        fontSize = 30.sp,
+                    )
+                    Button(onClick = {count++}) {
+                        Text(text = "Click Me")
+                    }
+
+                }
+
+            }
+        }
+    }
+}
