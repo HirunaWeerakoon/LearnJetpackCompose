0ec3b44d8e9964ea0c29380f0e26bab5a2ca2efc
HirunaWeerakoon <it24610788@my.sliit.lk>
2025-12-05 12:11:24 +0530

    first app UI

--- app/src/main/java/com/example/google_learning/MainActivity.kt
+++ app/src/main/java/com/example/google_learning/MainActivity.kt
@@ -7,13 +7,22 @@ import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.benchmark.traceprocessor.Row
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -35,25 +44,56 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Google_learningTheme {
-                var count by remember{
-                    mutableStateOf(0)
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
                 }
                 Column(
-                    modifier= Modifier
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Text(
-                        text=count.toString(),
-                        fontSize = 30.sp,
-                    )
-                    Button(onClick = {count++}) {
-                        Text(text = "Click Me")
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+
+                ) {
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { text ->
+                                name = text
+                            },
+                            modifier = Modifier.weight(1f),
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(
+                            onClick = {
+                                if (name.isNotBlank()) {
+                                    names = names + name
+                                    name = ""
+                                }
+                            },
+                        ) {
+                            Text(text = "Add")
+                        }
+
+
                     }
-
+                    LazyColumn {
+                        items(names.size) { index ->
+                            Text(
+                                text = names[index],
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            )
+                            HorizontalDivider()
+                        }
+                    }
                 }
-
             }
         }
     }
